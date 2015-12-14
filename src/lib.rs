8d99d541727b685c8d59a72416fd12f08a7c0e71
softprops <d.tangren@gmail.com>
2015-12-14 13:13:43 +0900

    apply rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -20,8 +20,8 @@ extern crate libc;
 /// returns true if this is a tty
 #[cfg(unix)]
 pub fn is() -> bool {
-  let r = unsafe { libc::isatty(libc::STDOUT_FILENO) };
-  r != 0
+    let r = unsafe { libc::isatty(libc::STDOUT_FILENO) };
+    r != 0
 }
 
 /// returns true if this is a tty
@@ -31,18 +31,16 @@ pub fn is() -> bool {
     extern crate winapi;
     use std::ptr;
     let handle: winapi::HANDLE = unsafe {
-        kernel32::CreateFileA(
-            b"CONOUT$\0".as_ptr() as *const i8,
-            winapi::GENERIC_READ | winapi::GENERIC_WRITE,
-            winapi::FILE_SHARE_WRITE,
-            ptr::null_mut(),
-            winapi::OPEN_EXISTING,
-            0,
-            ptr::null_mut(),
-            )
+        kernel32::CreateFileA(b"CONOUT$\0".as_ptr() as *const i8,
+                              winapi::GENERIC_READ | winapi::GENERIC_WRITE,
+                              winapi::FILE_SHARE_WRITE,
+                              ptr::null_mut(),
+                              winapi::OPEN_EXISTING,
+                              0,
+                              ptr::null_mut())
     };
     if handle == winapi::INVALID_HANDLE_VALUE {
-        println!("handle was invalid");
+        return false;
     }
     let mut out = 0;
     unsafe { kernel32::GetConsoleMode(handle, &mut out) != 0 }
@@ -50,7 +48,7 @@ pub fn is() -> bool {
 
 /// returns true if this is _not_ a tty
 pub fn isnt() -> bool {
-  !is()
+    !is()
 }
 
 #[cfg(test)]
